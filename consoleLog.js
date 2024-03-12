7f52557b9646cecfbeaa0af2ec3ecc1e25198cee
Yuri de Almeida Cabanha <yh.yuricabanha@gmail.com>
2024-03-12 11:08:52 -0300

    Nono commit

--- consoleLog.js
+++ consoleLog.js
@@ -1,10 +1,17 @@
-//var num = 0
-function compare(x){
-if (x<5) {
-    alert("Menor que cinco");
-} else if(x==5) {
-    alert("Igual a 5");
-} else {
-    alert("Maior que 5");
-}
+function compare(x) {
+    if (x == '') {
+        alert('O campo está vazio');
+        return false;
+    }
+    if (x < 5) {
+        if (x<0){
+            alert(x+" Menor que cinco e negativo");    
+        }else{
+            alert(" Menor que cinco");
+        }
+    } else if (x == 5) {
+        alert(x+" Igual a 5");
+    } else {
+        alert(x+" Maior que 5");
+    }
 }
