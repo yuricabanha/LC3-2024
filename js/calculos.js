6b7afb2ea052df545fc62791031b3ab409fbaf9a
Yuri de Almeida Cabanha <yh.yuricabanha@gmail.com>
2024-03-18 11:07:46 -0300

    Décimo primeiro

---
+++ js/calculos.js
@@ -0,0 +1,11 @@
+
+function calculo(x,y){
+    soma = x+y;
+    alert("a soma de "+x+" + "+y+" é igual a "+soma);
+    divisao = x/y;
+    alert("a divisão de "+x+" / "+y+" é igual a "+divisao);
+    subtracao = x-y;
+    alert("a subtração de "+x+" - "+y+" é igual a "+subtracao);
+    multiplicacao = x*y;
+    alert("a multiplicação de "+x+" * "+y+" é igual a "+multiplicacao);
+}
